ef4af87261c5a12e458ef29eb1aa45d12e1c0969
Larissa Ganaha <larissa.ganaha@ifood.com.br>
2021-11-10 11:55:28 -0300

    Move test logic to a separate type `HTTPClientSpy`

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -9,30 +9,37 @@ import XCTest
 
 class RemoteFeedLoader {
     func load() {
-        HTTPClient.shared.requestedURL = URL(string: "http://a-url.com")
+        HTTPClient.shared.get(from: URL(string: "http://a-url.com")!)
     }
 }
 
-// Singleton - Concrete type
+// Not a Singleton anymore
 class  HTTPClient {
-    static let shared = HTTPClient() // Single point of access
+    static var shared = HTTPClient() // Global State
 
-    private init() {}
+    func get(from url: URL) {}
+}
+
+class HTTPClientSpy: HTTPClient {   // Test Type
+    override func get(from url: URL) {
+        requestedURL = url
+    }
 
     var requestedURL: URL?
-
 }
 
 class RemoteFeedLoaderTests: XCTestCase {
     func test_init_doesNotRequestDateFromURL() {
-        let client = HTTPClient.shared // Concrete type reference
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         _ = RemoteFeedLoader()
 
         XCTAssertNil(client.requestedURL)
     }
 
     func test_load_requestDataFromURL() {
-        let client = HTTPClient.shared // Concrete type reference
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         let sut = RemoteFeedLoader()
 
         sut.load()
