7efd4d039efcb040ecd640c25930c137b428c1f2
Larissa Ganaha <larissa.ganaha@ifood.com.br>
2021-12-30 12:09:16 -0300

    Load command fails on retrieval error

--- EssentialFeedTests/Feed Cache/Helpers/FeedStoreSpy.swift
+++ EssentialFeedTests/Feed Cache/Helpers/FeedStoreSpy.swift
@@ -19,6 +19,7 @@ public  class FeedStoreSpy: FeedStore {
 
     private var deletionCompletions = [DeletionCompletion]()
     private var insertionCompletions = [InsertionCompletion]()
+    private var retrievalCompletions = [RetrievalCompletion]()
 
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
         deletionCompletions.append(completion)
@@ -46,7 +47,12 @@ public  class FeedStoreSpy: FeedStore {
         insertionCompletions[index](nil)
     }
 
-    public func retrieve() {
+    public func retrieve(completion: @escaping RetrievalCompletion) {
+        retrievalCompletions.append(completion)
         receivedMessages.append(.retrieve)
+    }
+
+    func completeRetrieval(with error: Error, at index: Int = 0) {
+        retrievalCompletions[index](error)
     }
 }
--- EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -18,9 +18,26 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
     func test_load_requestsCacheRetrieval() {
         let (sut, store) = makeSUT()
 
-        sut.load()
+        sut.load { _ in }
 
         XCTAssertEqual(store.receivedMessages, [.retrieve])
+    }
+
+    func test_load_failsOnRetrievalError() {
+        let (sut, store) = makeSUT()
+        let expectation = expectation(description: "Wait for load completion")
+        let retrievalError = anyNSError()
+
+        var receivedError: Error?
+        sut.load { error in
+            receivedError = error
+            expectation.fulfill()
+        }
+
+        store.completeRetrieval(with: retrievalError)
+        wait(for: [expectation], timeout: 1.0)
+
+        XCTAssertEqual(receivedError as NSError?, retrievalError)
     }
 
     // MARK: - Helpers
@@ -31,5 +48,9 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
         trackForMemoryLeaks(store, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, store)
+    }
+
+    private func anyNSError() -> NSError {
+        NSError(domain: "any error", code: 0)
     }
 }
