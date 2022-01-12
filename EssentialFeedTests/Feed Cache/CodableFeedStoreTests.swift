cbed9c767b2d8bee7de67be0b111dfde3dee3965
Larissa Ganaha <larissa.ganaha@ifood.com.br>
2022-01-12 10:12:03 -0300

    Extract reusable FeedStoreSpecs helper methods into a shared scope so it can be used by other `FeedStore` implementation tests

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -180,19 +180,6 @@ class CodableFeedStoreTests: XCTestCase, FailableFeedStore {
         return sut
     }
 
-    @discardableResult
-    private func insert(_ cache: (feed: [LocalFeedImage], timestamp: Date), to sut: FeedStore) -> Error? {
-        let expectation = expectation(description: "Wait for cache insertion")
-        var insertionError: Error?
-        sut.insert(cache.feed, timestamp: cache.timestamp) {  receivedInsertionError in
-            insertionError = receivedInsertionError
-            expectation.fulfill()
-        }
-
-        wait(for: [expectation], timeout: 1.0)
-        return insertionError
-    }
-
     private func testSpecificStoreURL() -> URL {
         cachesDirectory().appendingPathComponent("\(type(of: self)).store")
     }
@@ -208,41 +195,6 @@ class CodableFeedStoreTests: XCTestCase, FailableFeedStore {
     private func deleteStoreArtifacts() {
         try? FileManager.default.removeItem(at: testSpecificStoreURL())
     }
-
-    private func expect(_ sut: FeedStore, toRetrieveTwice expectedResult: RetrievedCachedResult, file: StaticString = #filePath, line: UInt = #line) {
-        expect(sut, toRetrieve: expectedResult)
-        expect(sut, toRetrieve: expectedResult)
-    }
-
-    private func expect(_ sut: FeedStore, toRetrieve expectedResult: RetrievedCachedResult, file: StaticString = #filePath, line: UInt = #line) {
-        let expectation = expectation(description: "Wait for cache retrieval")
-        sut.retrieve { retrievedResult in
-            switch (expectedResult, retrievedResult) {
-            case (.empty, .empty),
-                (.failure, .failure):
-                break
-            case let (.found(expected), .found(retrieved)):
-                XCTAssertEqual(retrieved.feed, expected.feed, file: file, line: line)
-                XCTAssertEqual(retrieved.timestamp, expected.timestamp, file: file, line: line)
-            default:
-                XCTFail("Expected to retrieve \(expectedResult), got \(retrievedResult) instead", file: file, line: line)
-            }
-            expectation.fulfill()
-        }
-
-        wait(for: [expectation], timeout: 1.0)
-    }
-
-    private func deleteCache(from sut: FeedStore) -> Error? {
-         let expectation = expectation(description: "Wait for cache deletion")
-         var deletionError: Error?
-         sut.deleteCachedFeed { receivedDeletionError in
-             deletionError = receivedDeletionError
-             expectation.fulfill()
-         }
-         wait(for: [expectation], timeout: 1.0)
-         return deletionError
-     }
 
     private func cachesDirectory() -> URL {
         FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
