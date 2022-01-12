0c68efb41ab8500adc0e6df53e64f7e7ac27a020
Larissa Ganaha <larissa.ganaha@ifood.com.br>
2022-01-12 20:45:14 -0300

    Proved that CoreDataFeedStore side-effects run serially and `CoreDataFeedStore.deleteCachedFeed()` empties previously inserted cache

--- EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -71,11 +71,15 @@ import XCTest
     }
 
     func test_delete_emptiesPreviouslyInsertedCache() {
+        let sut = makeSUT()
 
+        assertThatDeleteEmptiesPreviouslyInsertedCache(on: sut)
     }
 
     func test_storeSideEffects_runSerially() {
+        let sut = makeSUT()
 
+        assertThatSideEffectsRunSerially(on: sut)
     }
 
     // - MARK: Helpers
