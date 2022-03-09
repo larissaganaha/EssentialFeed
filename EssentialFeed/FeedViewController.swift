869e046c28de14d289d2f69b65faf1582fa9daf4
Larissa Ganaha <larissaganaha@gmail.com>
2022-03-09 10:55:08 -0300

    Move `FeedViewController` code to production target

--- EssentialFeed/FeedViewController.swift
+++ EssentialFeed/FeedViewController.swift
@@ -17,10 +17,25 @@ struct FeedImageViewModel {
 class FeedViewController: UITableViewController {
 
     private var feed = [FeedImageViewModel]()
+    private var loader: FeedLoader?
+
+    convenience init(loader: FeedLoader) {
+        self.init()
+        self.loader = loader
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
+        load()
+    }
+
+    @objc private func load() {
+        refreshControl?.beginRefreshing()
+        loader?.load { [weak self] _ in
+            self?.refreshControl?.endRefreshing()
+        }
     }
 
     override func viewWillAppear(_ animated: Bool) {
--- EssentialFeedTests/FeedViewControllerTests.swift
+++ EssentialFeedTests/FeedViewControllerTests.swift
@@ -4,31 +4,9 @@
 //
 //  Created by Larissa Ganaha on 09/03/22.
 //
-import EssentialFeed
+@testable import EssentialFeed
 import UIKit
 import XCTest
-
-final class FeedViewController: UITableViewController {
-    private var loader: FeedLoader?
-
-    convenience init(loader: FeedLoader) {
-        self.init()
-        self.loader = loader
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        refreshControl = UIRefreshControl()
-        refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
-        load()
-    }
-    @objc private func load() {
-        refreshControl?.beginRefreshing()
-        loader?.load { [weak self] _ in
-            self?.refreshControl?.endRefreshing()
-        }
-    }
-}
 
 final class FeedViewControllerTests: XCTestCase {
 
